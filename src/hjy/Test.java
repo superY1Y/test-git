e3fb9579eb255241416db7a55704aed44f4e0dd9
hjy <2274065780@qq.com>
2023-10-01 16:29:14 +0800

    v4

--- src/hjy/Test.java
+++ src/hjy/Test.java
@@ -1,7 +1,7 @@
 package hjy;
 
 public class Test {
-    public static void main(){
+    public static void main(String[] args){
         System.out.println("nihao");
         System.out.println("wassup");
         System.out.println("iwanttotalkwithyou");
