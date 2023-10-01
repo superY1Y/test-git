6437cc17e9f7cb7e1cab3a4cc931f7d69774d91d
hjy <2274065780@qq.com>
2023-10-01 16:24:53 +0800

    v3

--- src/hjy/Test.java
+++ src/hjy/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(){
         System.out.println("nihao");
         System.out.println("wassup");
+        System.out.println("iwanttotalkwithyou");
     }
 }
